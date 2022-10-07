a64184299a3902b1ef6097e2b703a4eeed294c1e
Malte Alexander Koefoed Schalck <malsc@itu.dk>
2022-10-07 13:50:15 +0200

    test af legendary og ost

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -2,11 +2,6 @@ namespace GildedRose.Tests;
 
 public class ProgramTests
 {
-    [Fact]
-    public void TestTheTruth()
-    {
-        true.Should().BeTrue();
-    }
 
     [Fact]
     public void normalitem_sellin_and_quality_after_1_updatequality(){
@@ -21,8 +16,44 @@ public class ProgramTests
         int SellInoutput = program.Items[0].SellIn;
         int Qualityoutput = program.Items[0].Quality;
 
-        Assert.Equal(SellInoutput,9);
-        Assert.Equal(Qualityoutput,9);
+        Assert.Equal(9,SellInoutput);
+        Assert.Equal(9,Qualityoutput);
+
+    }
+
+    [Fact]
+    public void cheeseitem_sellin_and_quality_after_1_updatequality(){
+
+        var program = new Program(){
+            Items = new List<Item>(){
+            new Item{Name = "Aged Brie",SellIn = 10, Quality= 10}
+            }
+        };
+
+        program.UpdateQuality();
+        int SellInoutput = program.Items[0].SellIn;
+        int Qualityoutput = program.Items[0].Quality;
+
+        Assert.Equal(9,SellInoutput);
+        Assert.Equal(11,Qualityoutput);
+
+    }
+
+    [Fact]
+    public void Legendary_sellin_and_quality_after_1_updatequality(){
+
+        var program = new Program(){
+            Items = new List<Item>(){
+            new Item{Name = "Sulfuras, Hand of Ragnaros",SellIn = 10, Quality= 10}
+            }
+        };
+
+        program.UpdateQuality();
+        int SellInoutput = program.Items[0].SellIn;
+        int Qualityoutput = program.Items[0].Quality;
+
+        Assert.Equal(10,SellInoutput);
+        Assert.Equal(10,Qualityoutput);
 
     }
 }
